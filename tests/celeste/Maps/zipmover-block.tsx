165d13596ac2bd1cb794358fb16071539bbdba6d
Shawn Presser <shawnpresser@gmail.com>
2019-03-04 09:41:59 -0600

    chapter1 map work

---
+++ tests/celeste/Maps/zipmover-block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="zipmover-block" tilewidth="8" tileheight="8" tilecount="9" columns="3">
+ <image source="../Graphics/Atlases/Gameplay/objects/zipmover/block.png" width="24" height="24"/>
+</tileset>
---
+++ tests/celeste/Maps/zipmover.tsx
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="zipmover" tilewidth="10" tileheight="12" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/cog.png"/>
+ </tile>
+ <tile id="1">
+  <image width="8" height="12" source="../Graphics/Atlases/Gameplay/objects/zipmover/light00.png"/>
+ </tile>
+ <tile id="2">
+  <image width="8" height="12" source="../Graphics/Atlases/Gameplay/objects/zipmover/light01.png"/>
+ </tile>
+ <tile id="3">
+  <image width="8" height="12" source="../Graphics/Atlases/Gameplay/objects/zipmover/light02.png"/>
+ </tile>
+ <tile id="4">
+  <image width="8" height="12" source="../Graphics/Atlases/Gameplay/objects/zipmover/light03.png"/>
+ </tile>
+ <tile id="5">
+  <image width="8" height="8" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog00.png"/>
+ </tile>
+ <tile id="6">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog01.png"/>
+ </tile>
+ <tile id="7">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog02.png"/>
+ </tile>
+ <tile id="8">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog03.png"/>
+ </tile>
+ <tile id="9">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog04.png"/>
+ </tile>
+ <tile id="10">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog05.png"/>
+ </tile>
+ <tile id="11">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog06.png"/>
+ </tile>
+ <tile id="12">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog07.png"/>
+ </tile>
+ <tile id="13">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog08.png"/>
+ </tile>
+ <tile id="14">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog09.png"/>
+ </tile>
+ <tile id="15">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog10.png"/>
+ </tile>
+ <tile id="16">
+  <image width="10" height="10" source="../Graphics/Atlases/Gameplay/objects/zipmover/innercog11.png"/>
+ </tile>
+</tileset>
