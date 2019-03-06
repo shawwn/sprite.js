cc802196ecd716d53988b266c4e7f5e725bfa0fc
Shawn Presser <shawnpresser@gmail.com>
2019-03-06 08:51:17 -0600

    Add stardew assets

---
+++ tests/celeste/Maps/stardewOutdoorsSpring.tsx
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="stardewOutdoorsSpring" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="../Graphics/Atlases/Gameplay/tilesets/stardewOutdoorsSpring.png" width="400" height="1264"/>
+ <terraintypes>
+  <terrain name="dirt" tile="0"/>
+  <terrain name="grass" tile="0"/>
+  <terrain name="mud" tile="0"/>
+  <terrain name="grass2" tile="0"/>
+  <terrain name="water" tile="0"/>
+ </terraintypes>
+ <tile id="125">
+  <objectgroup draworder="index">
+   <object id="1" x="46" y="12">
+    <polygon points="0,0 -57,-64 -85,62 31,55"/>
+   </object>
+   <object id="2" x="87" y="-175">
+    <polygon points="0,0 -3,83 51,26"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="153" terrain="0,0,0,0"/>
+ <tile id="175" terrain="1,1,1,1"/>
+ <tile id="176" terrain="0,0,0,0"/>
+ <tile id="177" terrain="0,1,0,0"/>
+ <tile id="178" terrain="0,0,0,1"/>
+ <tile id="183" terrain="0,0,0,4"/>
+ <tile id="184" terrain="0,0,4,4"/>
+ <tile id="185" terrain="0,0,4,0"/>
+ <tile id="200" terrain="1,1,1,0"/>
+ <tile id="201" terrain="1,1,0,0"/>
+ <tile id="202" terrain="1,0,0,0"/>
+ <tile id="203" terrain="1,1,0,1"/>
+ <tile id="208" terrain="0,4,0,4"/>
+ <tile id="209" terrain="4,4,4,4"/>
+ <tile id="210" terrain="4,0,4,0"/>
+ <tile id="225" terrain="1,0,1,0"/>
+ <tile id="226" terrain="0,0,0,0"/>
+ <tile id="227" terrain="0,0,0,0"/>
+ <tile id="228" terrain="0,1,0,1"/>
+ <tile id="233" terrain="0,4,0,4"/>
+ <tile id="234" terrain="4,4,4,4"/>
+ <tile id="235" terrain="4,0,4,0"/>
+ <tile id="250" terrain="1,0,1,1"/>
+ <tile id="251" terrain="0,0,1,1"/>
+ <tile id="252" terrain="0,0,1,0"/>
+ <tile id="253" terrain="0,1,1,1"/>
+ <tile id="258" terrain="0,4,0,0"/>
+ <tile id="259" terrain="4,4,0,0"/>
+ <tile id="260" terrain="4,0,0,0"/>
+ <tile id="275" terrain="1,1,1,1"/>
+ <tile id="325" terrain="1,1,1,3"/>
+ <tile id="326" terrain="1,1,3,3"/>
+ <tile id="327" terrain="1,1,3,1"/>
+ <tile id="328" terrain="1,3,3,3"/>
+ <tile id="350" terrain="1,3,1,3"/>
+ <tile id="351" terrain="3,3,3,3"/>
+ <tile id="352" terrain="3,1,3,1"/>
+ <tile id="353" terrain="3,3,3,1"/>
+ <tile id="375" terrain="1,3,1,1"/>
+ <tile id="376" terrain="3,3,1,1"/>
+ <tile id="377" terrain="3,1,1,1"/>
+ <tile id="378" terrain="3,1,3,3"/>
+ <tile id="402" terrain="1,1,1,1"/>
+ <tile id="403" terrain="3,3,1,3"/>
+ <tile id="433" terrain="0,0,0,0"/>
+ <tile id="530" terrain="0,0,0,2"/>
+ <tile id="531" terrain="0,0,2,2"/>
+ <tile id="532" terrain="0,0,2,0"/>
+ <tile id="555" terrain="0,2,0,2"/>
+ <tile id="556" terrain="2,2,2,2"/>
+ <tile id="557" terrain="2,0,2,0"/>
+ <tile id="580" terrain="0,2,0,0"/>
+ <tile id="581" terrain="2,2,0,0"/>
+ <tile id="582" terrain="2,0,0,0"/>
+ <tile id="630" terrain="2,2,2,2"/>
+ <tile id="631" terrain="2,1,2,2"/>
+ <tile id="632" terrain="2,2,2,1"/>
+ <tile id="654" terrain="1,1,1,2"/>
+ <tile id="655" terrain="1,1,2,2"/>
+ <tile id="656" terrain="1,2,2,2"/>
+ <tile id="657" terrain="1,1,2,1"/>
+ <tile id="679" terrain="1,2,1,2"/>
+ <tile id="680" terrain="2,2,2,2"/>
+ <tile id="681" terrain="2,2,2,2"/>
+ <tile id="682" terrain="2,1,2,1"/>
+ <tile id="704" terrain="1,2,1,1"/>
+ <tile id="705" terrain="2,2,1,1"/>
+ <tile id="706" terrain="2,2,1,2"/>
+ <tile id="707" terrain="2,1,1,1"/>
+ <tile id="708" terrain="2,2,2,4"/>
+ <tile id="709" terrain="2,2,4,4"/>
+ <tile id="710" terrain="2,2,4,2"/>
+ <tile id="733" terrain="2,4,2,4"/>
+ <tile id="734" terrain="4,4,4,4"/>
+ <tile id="735" terrain="4,2,4,2"/>
+ <tile id="758" terrain="2,4,2,4"/>
+ <tile id="759" terrain="4,4,4,4"/>
+ <tile id="760" terrain="4,2,4,2"/>
+ <tile id="783" terrain="2,4,2,2"/>
+ <tile id="784" terrain="4,4,2,2"/>
+ <tile id="785" terrain="4,2,2,2"/>
+</tileset>
