ac93962ca83b281786e826040cded612c8e8883f
Shawn Presser <shawnpresser@gmail.com>
2019-03-10 01:49:07 -0600

    Chapter 1 work

---
+++ tests/celeste/Maps/crumbleBlock.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="crumbleBlock" tilewidth="32" tileheight="8" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="6" source="../Graphics/Atlases/Gameplay/objects/crumbleBlock/outline.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="8" source="../Graphics/Atlases/Gameplay/objects/crumbleBlock/default.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="8" source="../Graphics/Atlases/Gameplay/objects/crumbleBlock/cliffside.png"/>
+ </tile>
+</tileset>
---
+++ tests/celeste/Maps/crumbleBlockDefault.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="crumbleBlockDefault" tilewidth="8" tileheight="8" tilecount="4" columns="4">
+ <image source="../Graphics/Atlases/Gameplay/objects/crumbleBlock/default.png" width="32" height="8"/>
+</tileset>
